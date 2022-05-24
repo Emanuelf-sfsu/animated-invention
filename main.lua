de9bf4bcbac8c8c8b350655843f7b4511e1116da
emanuelf-sfsu <efrancis1@mail.sfsu.edu>
2022-05-24 12:15:24 -0700

    pong-3 Rectangle update
    The rectangles (paddles) are able to move up and down but have no boundaries .

--- main.lua
+++ main.lua
@@ -14,6 +14,9 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+-- Paddle movement speed
+PADDLE_SPEED = 200
+
 -- Runs During games startup
 function love.load()
     -- love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{
@@ -26,6 +29,9 @@ function love.load()
     -- font to better replicate the retro version of pong
     smallFont = love.graphics.newFont('font.ttf', 8)
 
+    -- larger font for score
+    scoreFont = love.graphics.newFont('font.ttf',32)
+
     -- set LOVE2D to active font
     love.graphics.setFont(smallFont)
 
@@ -35,7 +41,39 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    -- initializing the scores
+    player1Score = 0
+    player2Score = 0
+
+    -- paddle positions on the Y axis (they can only move up or down)
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
 end
+
+function love.update(dt)
+    -- player1 movement
+    if love.keyboard.isDown('w') then
+        -- add negative paddle speed  
+        player1Y = player1Y + -PADDLE_SPEED * dt
+
+    elseif love.keyboard.isDown('s') then
+        -- add positive 
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        -- add negative paddle speed  
+        player2Y = player2Y + -PADDLE_SPEED * dt
+        
+    elseif love.keyboard.isDown('down') then
+        -- add positive 
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
+
+
 
 function love.keypressed(key)
     -- key can be called by name
@@ -52,12 +90,18 @@ function love.draw()
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)    
     -- welcome screen
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    -- draw score on the left and right of the screen
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
     
     -- render first paddle (left)
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     -- render second paddle (right)
-    love.graphics.rectangle('fill',VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill',VIRTUAL_WIDTH - 10, player2Y - 50, 5, 20)
 
     -- render ball (center)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4 )
