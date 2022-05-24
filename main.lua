d47668dab6f3205564cb1566827cc2fa7ed31948
emanuelf-sfsu <efrancis1@mail.sfsu.edu>
2022-05-23 17:43:48 -0700

    Pong-1
    Now using the push library, we get the old school pixel look.

--- main.lua
+++ main.lua
@@ -4,27 +4,48 @@
 -- I am following this tutorial to give me an insight on one of the many different 
 -- uses of C++.
 
+-- https://github.com/Ulydev/push
+push = require 'push'
 
 -- Game frame
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 -- Runs During games startup
 function love.load()
-    love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{
+    -- love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{
+    --     fullscreen = false,
+    --     resizable = false,
+    --     vsync = true
+    -- })
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    -- Will instead use the push libary 
+    push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH,WINDOW_HEIGHT,{
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
-function love.draw()
-    love.graphics.printf(
-        'Hello Pong!',
-        0,
-        WINDOW_HEIGHT/2 - 6,
-        WINDOW_WIDTH,
-        'center'
-    )
+function love.keypressed(key)
+    -- key can be called by name
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
+
+function love.draw()
+    -- begin rendering at virtual resolution
+    push:apply('start')
+
+
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end rendering at virtual resolution
+    push:apply('end')
+end
+
 
